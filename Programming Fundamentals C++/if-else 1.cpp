6a324e105ab7bbb25770ccc3dfb992c7af8d4971
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-05 13:18:22 +0500

    Create if-else 1.cpp

---
+++ Programming Fundamentals C++/if-else 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int marks;
+    cout << "Enter marks: ";
+    cin >> marks;
+    if (marks >= 90)
+        cout << "Grade A+" << endl;
+    else if (marks >= 80)
+        cout << "Grade A" << endl;
+    else if (marks >= 70)
+        cout << "Grade B" << endl;
+    else if (marks >= 60)
+        cout << "Grade C" << endl;
+    else
+        cout << "Fail" << endl;
+
+    return 0;
+}
