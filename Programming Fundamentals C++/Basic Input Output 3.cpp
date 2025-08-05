12e556bcaab330dfd36e68f468e28389393b87f4
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-05 13:09:10 +0500

    Create Basic Input Output 3.cpp

---
+++ Programming Fundamentals C++/Basic Input Output 3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    float length, width;
+    cout << "Enter length and width of the rectangle: ";
+    cin >> length >> width;
+    cout << "Area of rectangle is: " << length * width << endl;
+    return 0;
+}
