fb6e85b13b0557eb36a4e532c3ef362b18048796
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-06 17:05:21 +0500

    Create Function Basics 02.cpp

--- Programming Fundamentals C++/23- Function Basics 02.cpp
+++ Programming Fundamentals C++/23- Function Basics 02.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int factorial(int );
+int main(){
+	int n;
+	cout<<"Enter a number for factorial:";
+	cin>>n;
+	cout<<factorial(n)<<" is the factorial of "<<n<<"\n";
+	}
+	int factorial(int n){
+		int fact=1;
+		for(int i=1; i<=n; i++){
+			fact=fact*i;
+		}
+		return fact;
+	}
