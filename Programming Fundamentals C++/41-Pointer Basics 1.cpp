6049925b8ff6eed7e47798ee873bc6c8e418a2cb
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-07 17:18:58 +0500

    Create Pointer Basics 1.cpp

---
+++ Programming Fundamentals C++/41-Pointer Basics 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 10;
+    int* p = &a;
+    cout << "Value of a: " << a << endl;
+    cout << "Address of a: " << &a << endl;
+    cout << "Value stored in pointer: " << p << endl;
+    cout << "Value pointed by pointer: " << *p << endl;
+    return 0;
+}
