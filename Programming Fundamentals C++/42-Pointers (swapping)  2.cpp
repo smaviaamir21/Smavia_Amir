775715d4e2b427f39a3eb769518807829ca1bd1f
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-07 17:21:05 +0500

    Create Pointers Program 2.cpp

---
+++ Programming Fundamentals C++/42-Pointers (swapping)  2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 10, b = 20;
+    int* p1 = &a;
+    int* p2 = &b;
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0;
+}
