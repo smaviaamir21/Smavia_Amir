1ebb6309db2949defc5b53b2bd6f05bbc79ef1cb
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-07 16:07:51 +0500

    Create Array Program (Hit to Ship Game) 2D 8.cpp

---
+++ Programming Fundamentals C++/38-Array Program (Hit to Ship Game) 2D 8.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int index, sub_index, score=0;
+	int myGame[4][4]={{0,1,0,0},{1,1,0,0},{0,0,0,0},{0,0,0,1}};
+	while(score<4){
+		cout<<"Enter index and sub index to find out the ship:";
+		cin>>index>>sub_index;
+		if(myGame[index][sub_index]==1){
+			cout<<"Hit"<<endl<<3-score<<"ships left to hit."<<endl;
+			score++;
+			cout<<"Your score is:"<<score<<"now."<<endl;
+			myGame[index][sub_index]=0;
+		}
+		else{
+			cout<<"You missed it."<<endl;
+		}
+	}
+	return 0;
+}
